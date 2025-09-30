520c31a2e56d7046b6831a43a8d7c18dde795c3f
Ding-Dongen <pavado.pvd@gmail.com>
2025-09-30 14:20:09 +0200

    added cors

--- eventsystem/Program.cs
+++ eventsystem/Program.cs
@@ -4,6 +4,8 @@ using Data.Repositories;
 using Infrastructure.Interfaces;
 using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,17 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
+// ===== DataProtection (delad nyckelring mellan tjänster) =====
+var keysPath = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+    "CoreGym", "dp-keys"
+);
+Directory.CreateDirectory(keysPath);
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(keysPath))
+    .SetApplicationName("CoreGym"); // MÅSTE matcha AuthSystem
+
 builder.Services.AddScoped<ITrainingClassService, TrainingClassService>();
 builder.Services.AddScoped<ITrainingClassRepository, TrainingClassRepository>();
 
@@ -22,12 +35,24 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthorization();
 
+// ===== CORS: hårdkodat origin =====
+const string SpaCors = "spa";
+string[] allowedOrigins = { "https://fantasticg5-dmdbeshvcmfxe6ey.northeurope-01.azurewebsites.net", "http://localhost:5173", "https://localhost:5173" };
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(SpaCors, p => p
+        .WithOrigins(allowedOrigins)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
+});
 
 var app = builder.Build();
 
 
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseAuthentication();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
@@ -43,6 +68,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(SpaCors); 
 app.UseAuthorization();
 app.MapControllers();
 
